30f7ac0177e6ff4428f4fe9d61580d2dd1a86053
Julian Hofmann <info@julian-hofmann.net>
2025-06-05 09:11:05 +0000

    [TASK] Declare key for parent

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,13 +1,15 @@
 CREATE TABLE tx_oauth2_beuser_provider_configuration (
   parentid int(10) DEFAULT 0 NOT NULL,
   provider varchar(255) DEFAULT '',
-  identifier varchar(255) DEFAULT ''
+  identifier varchar(255) DEFAULT '',
+  KEY parent (parentid)
 );
 
 CREATE TABLE tx_oauth2_feuser_provider_configuration (
   parentid int(11) DEFAULT 0 NOT NULL,
   provider varchar(255) DEFAULT '',
-  identifier varchar(255) DEFAULT ''
+  identifier varchar(255) DEFAULT '',
+  KEY parent (parentid)
 );
 
 CREATE TABLE be_users (
